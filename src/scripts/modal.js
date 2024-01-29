cc75a831e2bd252da56397374436bf6cf82b3674
Breno Garcia <BrenoGHeat@gmail.com>
2024-01-29 15:06:10 -0300

    modal add

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,12 @@
 /* Desenvolva seu código aqui... */
+
+function handleModal(){
+    const register = document.querySelector("#form__register-link");
+    const modalContainer = document.querySelector("#modalContainer");
+
+    register.addEventListener("click" , () =>{
+        modalContainer.showModal();
+    })
+}
+
+handleModal();
